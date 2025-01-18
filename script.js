f657798161610792f02a209830d519bbcd6dbeb9
Shreya <106058735+shreyashetty-1@users.noreply.github.com>
2025-01-18 14:17:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission functionality for demonstration
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your message has been sent!");
+});
